977e82f76842fdb10982e7ee85716ee7d8fbd000
Manuel Maisano <manumaisano@gmail.com>
2024-04-02 13:28:24 +0200

    data app

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 import { slides } from "./data.js";
+
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+      return {
+        slides,
+        activeIndexSlide: 0,
+      };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  }).mount("#app");
